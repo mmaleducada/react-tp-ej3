fe05bccda57302ac7fff83f7a08056390a45ea22
Malena de Arriba <dearribamalena@gmail.com>
2023-06-01 01:03:01 -0300

    se realiza la logica para que el mensaje cambie de Hello my friend! a Hello my friende (from changed state)!

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
 const Titulo = ({ tituloNuevo }) => {
-  const [texto, setTexto] = useState("Hello my friend!");
+    const [texto, setTexto] = useState("Hello my friend!");
 
-  const hacerClick = () => {
-    setTexto(`Hello ${tituloNuevo} (from changed state)!`);
-  };
+    const hacerClick = () => {
+        setTexto(`Hello ${tituloNuevo} (from changed state)!`);
+    };
 
-  return (
-    <section className="container text-center">
-      <h1>
-        <strong>{texto}</strong>
-      </h1>
-      <button className="btn btn-info" onClick={hacerClick}>
-        Click here!
-      </button>
-    </section>
-  );
+    return (
+        <section className="container text-center">
+            <h1>
+                <strong>{texto}</strong>
+            </h1>
+            <button className="btn btn-info" onClick={hacerClick}>
+                Click here!
+            </button>
+        </section>
+    );
 };
 
 export default Titulo;
