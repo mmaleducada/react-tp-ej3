bdec5c73b243e2ab4ad0ef2ff6f50d10faa74d9d
Malena de Arriba <dearribamalena@gmail.com>
2023-06-01 01:01:36 -0300

    se agrega un boton que al hacer click cambia el mensaje inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Titulo from './components/Titulo'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
-      <Titulo tituloNuevo = "my friend!"></Titulo>
+      <Titulo tituloNuevo = "my friend"></Titulo>
     </>
   )
 }
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,9 +1,22 @@
-const Titulo = ({tituloNuevo}) => {
-    return (
-        <section className="container text-center">
-            <h1><strong>Hello {tituloNuevo} </strong></h1>
-        </section>
-    );
+import { useState } from "react";
+
+const Titulo = ({ tituloNuevo }) => {
+  const [texto, setTexto] = useState("Hello my friend!");
+
+  const hacerClick = () => {
+    setTexto(`Hello ${tituloNuevo} (from changed state)!`);
+  };
+
+  return (
+    <section className="container text-center">
+      <h1>
+        <strong>{texto}</strong>
+      </h1>
+      <button className="btn btn-info" onClick={hacerClick}>
+        Click here!
+      </button>
+    </section>
+  );
 };
 
 export default Titulo;
